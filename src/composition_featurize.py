83ee26a2d14c269391ee5208c5114a029220e320
Qilin GUO <kylinguo@icloud.com>
2024-05-31 09:26:18 +0800

    drop 'target_col_id' inplace

--- src/composition_featurize.py
+++ src/composition_featurize.py
@@ -1,4 +1,3 @@
-import multiprocessing
 import pandas as pd
 from matminer.featurizers.conversions import StrToComposition
 from my_composition_featurizer import MyCompositionFeaturizer
@@ -9,7 +8,7 @@ def composition_featurize(df: pd.DataFrame) -> pd.DataFrame:
     target_col_id = 'composition_pmg'
 
     ignore_errors = True
-    n_job = 24  # multiprocessing.cpu_count()
+    n_job = 24
 
     str_to_comp = StrToComposition(target_col_id=target_col_id)
     df_pmg: pd.DataFrame = str_to_comp.featurize_dataframe(df=df, col_id=source_col_id, ignore_errors=ignore_errors, pbar=True)
@@ -18,17 +17,20 @@ def composition_featurize(df: pd.DataFrame) -> pd.DataFrame:
     composition_featurizer.set_n_jobs(n_jobs=n_job)
 
     df_featurized: pd.DataFrame = composition_featurizer.featurize_dataframe(df=df_pmg, col_id=target_col_id, ignore_errors=ignore_errors, pbar=True)
-    df_featurized.drop(labels=[target_col_id, ], axis=1)
+    df_featurized.drop(labels=[target_col_id, ], axis=1, inplace=True)
     return df_featurized
 
 
 def test_composition_featurize():
-    data_file_input = '../data/data.json'
-    data_file_output = '../data/data_featurized.json'
+    data_file_input = 'data/data.json'
+    data_file_output_json = 'data/data_featurized.json'
+    data_file_output_xlsx = 'data/data_featurized.xlsx'
+
     df = pd.read_json(data_file_input)
     df_featurized = composition_featurize(df=df)
-    print(df_featurized)
-    df_featurized.to_json(path_or_buf=data_file_output, orient='records', indent=4)
+
+    df_featurized.to_excel(excel_writer=data_file_output_xlsx, index=False)
+    df_featurized.to_json(path_or_buf=data_file_output_json, orient='records', indent=4)  # Aborted (core dumped)
 
 
 def main():
